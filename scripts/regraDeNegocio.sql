8f58e64728db3494e14893413ed3ff14ef8863c1
Kauã Machado Grathwohl <kaua.grathwohl@thomsonreuters.com>
2023-12-07 11:22:34 -0300

    Criação das perguntas de regra de negócio

---
+++ scripts/regraDeNegocio.sql
@@ -0,0 +1,19 @@
+-- 1) Utilizando a linguagem SQL, execute consultas que possam responder a seguinte questão: Qual locatário possui mais contratos de aluguel vinculado? 
+
+
+
+
+
+-- 2) Utilizando a linguagem SQL, execute consultas que possam responder a seguinte questão: Qual proprietário possui mais imóveis alugados ?
+
+
+
+-- 3) Utilizando a linguagem SQL, execute consultas que possam responder a seguinte questão: Qual o estado que tem mais imobiliárias residindo?
+
+
+
+-- 4) Utilizando a linguagem SQL, execute consultas que possam responder a seguinte questão: Quantos contratos serão finalizados no ano de 2023? 
+
+
+
+-- 5) Utilizando a linguagem SQL, execute consultas que possam responder a seguinte questão: Qual imobiliaria realizou mais contratos nos ultimos dois anos?
