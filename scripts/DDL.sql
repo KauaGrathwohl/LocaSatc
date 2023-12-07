fa400a7304d693db4b39ffffe62dd448bf6d97b0
Gustavo Goulart <66621683+gosttavo@users.noreply.github.com>
2023-12-07 01:04:19 -0300

    Update and rename createTables.sql to DDL.sql

---
+++ scripts/DDL.sql
@@ -0,0 +1,127 @@
+CREATE TABLE IF NOT EXISTS `contratos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `imovel` int(11) NOT NULL,
+  `proprietario` int(11) NOT NULL,
+  `locatario` int(11) NOT NULL,
+  `corretor` int(11) NOT NULL,
+  `unidade` int(11) NOT NULL,
+  `dt_inicio` date NOT NULL,
+  `dt_fim` date NOT NULL,
+  `fiador` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32771 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `corretor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cargo` varchar(75) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `unidade` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4096 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `enderecos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCR
+
+CREATE TABLE IF NOT EXISTS `fiador` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` bigint(20) NOT NULL,
+  `contrato` int(11) DEFAULT NULL,
+  `renda` float NOT NULL,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2048 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `franqueado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `telefone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1024 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `imobiliaria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `franqueado` int(11) NOT NULL,
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `imoveis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `area` int(11) DEFAULT NULL,
+  `urbano` tinyint(1) DEFAULT NULL,
+  `quartos` int(11) DEFAULT NULL,
+  `banheiros` int(11) DEFAULT NULL,
+  `vaga_estacionamento` int(11) DEFAULT NULL,
+  `andar` int(11) DEFAULT NULL,
+  `aceita_animais` tinyint(1) DEFAULT NULL,
+  `mobiliada` tinyint(1) DEFAULT NULL,
+  `condominio` int(11) DEFAULT NULL,
+  `aluguel` int(11) DEFAULT NULL,
+  `iptu` int(11) DEFAULT NULL,
+  `seguro` int(11) DEFAULT NULL,
+  `proprietario` int(11) DEFAULT NULL,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16774 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `locatario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` bigint(20) NOT NULL,
+  `renda` float DEFAULT NULL,
+  `fiador` int(11) DEFAULT NULL,
+  `contrato` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16384 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `pessoas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telefone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
+
+CREATE TABLE IF NOT EXISTS `proprietario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` bigint(20) NOT NULL,
+  `contrato` int(11) DEFAULT NULL,
+  `uf` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `endereco` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=851956 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
